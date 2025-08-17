829211e467795c03bd9692ae17f987b16ec3bcff
MughniRayhan <tishtisha13@gmail.com>
2025-08-18 03:16:11 +0600

    solve problem5

---
+++ problem5.js
@@ -0,0 +1,12 @@
+const removeDuplicates = (arr) => {
+    const uniqueArray = [];
+    for( a of arr) {
+        if(!uniqueArray.includes(a)) {
+            uniqueArray.push(a);
+        }
+    }
+    return uniqueArray;
+}
+
+const arr = [1, 2, 2, 3, 4, 4];
+console.log(removeDuplicates(arr));
